bdcd501a1ef0cb24e4449dc9cab96f8c22ba58c2
Ankush Gusain <116104789+Ankushgusain99@users.noreply.github.com>
2024-08-08 21:22:20 +0530

    my first commit

--- backend/src/routes/allRoutes.js
+++ backend/src/routes/allRoutes.js
@@ -1,8 +1,10 @@
 const express=require('express')
 const router=express.Router()
-const { registerUser } = require("../controllers/auth");
+const { registerUser, loginUser, getAllUsers ,updateUser,deleteUser} = require("../controllers/auth");
 
 router.post('/registerUser',registerUser)
-
-
+router.post('/loginUser',loginUser)
+router.get('/getAllUsers',getAllUsers)
+router.put('/updateUser/:id',updateUser)
+router.delete('/deleteUser/:id',deleteUser)
 module.exports=router
